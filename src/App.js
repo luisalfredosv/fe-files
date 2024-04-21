5398619503d3f98dce99b843eb79cde5c28989df
Luis Sarabia <luissarabia80@gmail.com>
2024-04-21 08:44:40 -0400

    inicialización del proyecto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Container, Navbar } from "react-bootstrap";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<Navbar expand='lg' className='bg-body-tertiary'>
+				<Container>
+					<Navbar.Brand href='#home'>React Test App</Navbar.Brand>
+					<Navbar.Toggle aria-controls='basic-navbar-nav' />
+				</Container>
+			</Navbar>
+		</div>
+	);
 }
 
 export default App;
